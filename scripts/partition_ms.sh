7711e8ecf1cccc7f8a69c503a776944151e6b51f
tanony <tengdj@vip.qq.com>
2020-09-01 23:07:29 -0400

    minor update

--- scripts/partition_ms.sh
+++ scripts/partition_ms.sh
@@ -1,12 +1,11 @@
 
-../build/point_in_polygon -s /data/gisdata/raster/dat/all_source_100.dat -t /data/gisdata/raster/dat/all_point.dat -b 500 -v 5 -q -a --partition_only &&
-../build/point_in_polygon -s /data/gisdata/raster/dat/all_source_100.dat -t /data/gisdata/raster/dat/all_point.dat -b 500 -v 10 -q -a --partition_only &&
-../build/point_in_polygon -s /data/gisdata/raster/dat/all_source_100.dat -t /data/gisdata/raster/dat/all_point.dat -b 500 -v 20 -q -a --partition_only &&
-../build/point_in_polygon -s /data/gisdata/raster/dat/all_source_100.dat -t /data/gisdata/raster/dat/all_point.dat -b 500 -v 30 -q -a --partition_only &&
-../build/point_in_polygon -s /data/gisdata/raster/dat/all_source_100.dat -t /data/gisdata/raster/dat/all_point.dat -b 500 -v 40 -q -a --partition_only &&
-../build/point_in_polygon -s /data/gisdata/raster/dat/all_source_100.dat -t /data/gisdata/raster/dat/all_point.dat -b 500 -v 50 -q -a --partition_only &&
-../build/point_in_polygon -s /data/gisdata/raster/dat/all_source_100.dat -t /data/gisdata/raster/dat/all_point.dat -b 500 -v 60 -q -a --partition_only &&
-../build/point_in_polygon -s /data/gisdata/raster/dat/all_source_100.dat -t /data/gisdata/raster/dat/all_point.dat -b 500 -v 70 -q -a --partition_only &&
-../build/point_in_polygon -s /data/gisdata/raster/dat/all_source_100.dat -t /data/gisdata/raster/dat/all_point.dat -b 500 -v 80 -q -a --partition_only &&
-../build/point_in_polygon -s /data/gisdata/raster/dat/all_source_100.dat -t /data/gisdata/raster/dat/all_point.dat -b 500 -v 90 -q -a --partition_only &&
-../build/point_in_polygon -s /data/gisdata/raster/dat/all_source_100.dat -t /data/gisdata/raster/dat/all_point.dat -b 500 -v 100 -q -a --partition_only
+../build/point_in_polygon -s /data/gisdata/raster/dat/all_source_100.dat -t /data/gisdata/raster/dat/all_point.dat -b 400000 -v 10 -q --raster_only &&
+../build/point_in_polygon -s /data/gisdata/raster/dat/all_source_100.dat -t /data/gisdata/raster/dat/all_point.dat -b 400000 -v 20 -q --raster_only &&
+../build/point_in_polygon -s /data/gisdata/raster/dat/all_source_100.dat -t /data/gisdata/raster/dat/all_point.dat -b 400000 -v 30 -q --raster_only &&
+../build/point_in_polygon -s /data/gisdata/raster/dat/all_source_100.dat -t /data/gisdata/raster/dat/all_point.dat -b 400000 -v 40 -q --raster_only &&
+../build/point_in_polygon -s /data/gisdata/raster/dat/all_source_100.dat -t /data/gisdata/raster/dat/all_point.dat -b 400000 -v 50 -q --raster_only &&
+../build/point_in_polygon -s /data/gisdata/raster/dat/all_source_100.dat -t /data/gisdata/raster/dat/all_point.dat -b 400000 -v 60 -q --raster_only &&
+../build/point_in_polygon -s /data/gisdata/raster/dat/all_source_100.dat -t /data/gisdata/raster/dat/all_point.dat -b 400000 -v 70 -q --raster_only &&
+../build/point_in_polygon -s /data/gisdata/raster/dat/all_source_100.dat -t /data/gisdata/raster/dat/all_point.dat -b 400000 -v 80 -q --raster_only &&
+../build/point_in_polygon -s /data/gisdata/raster/dat/all_source_100.dat -t /data/gisdata/raster/dat/all_point.dat -b 400000 -v 90 -q --raster_only &&
+../build/point_in_polygon -s /data/gisdata/raster/dat/all_source_100.dat -t /data/gisdata/raster/dat/all_point.dat -b 400000 -v 100 -q --raster_only
