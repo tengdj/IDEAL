4e8356db314364c408fe08bb7c3df85eee76f23d
tengdj <tengdj@vip.qq.com>
2020-12-25 18:17:02 -0500

    minor update

--- src/geometry/query.cpp
+++ src/geometry/query.cpp
@@ -35,66 +35,6 @@ bool MyPolygon::contain(Point p){
 }
 
 
-
-// Given three colinear points p, q, r, the function checks if
-// point q lies on line segment 'pr'
-bool onSegment(Point p, Point q, Point r)
-{
-    if (q.x <= max(p.x, r.x) && q.x >= min(p.x, r.x) &&
-        q.y <= max(p.y, r.y) && q.y >= min(p.y, r.y))
-       return true;
-
-    return false;
-}
-
-// To find orientation of ordered triplet (p, q, r).
-// The function returns following values
-// 0 --> p, q and r are colinear
-// 1 --> Clockwise
-// 2 --> Counterclockwise
-int orientation(Point p, Point q, Point r)
-{
-    // See https://www.geeksforgeeks.org/orientation-3-ordered-points/
-    // for details of below formula.
-    int val = (q.y - p.y) * (r.x - q.x) -
-              (q.x - p.x) * (r.y - q.y);
-
-    if (val == 0) return 0;  // colinear
-
-    return (val > 0)? 1: 2; // clock or counterclock wise
-}
-
-// The main function that returns true if line segment 'p1q1'
-// and 'p2q2' intersect.
-bool doIntersect(Point p1, Point q1, Point p2, Point q2)
-{
-    // Find the four orientations needed for general and
-    // special cases
-    int o1 = orientation(p1, q1, p2);
-    int o2 = orientation(p1, q1, q2);
-    int o3 = orientation(p2, q2, p1);
-    int o4 = orientation(p2, q2, q1);
-
-    // General case
-    if (o1 != o2 && o3 != o4)
-        return true;
-
-    // Special Cases
-    // p1, q1 and p2 are colinear and p2 lies on segment p1q1
-    if (o1 == 0 && onSegment(p1, p2, q1)) return true;
-
-    // p1, q1 and q2 are colinear and q2 lies on segment p1q1
-    if (o2 == 0 && onSegment(p1, q2, q1)) return true;
-
-    // p2, q2 and p1 are colinear and p1 lies on segment p2q2
-    if (o3 == 0 && onSegment(p2, p1, q2)) return true;
-
-     // p2, q2 and q1 are colinear and q1 lies on segment p2q2
-    if (o4 == 0 && onSegment(p2, q1, q2)) return true;
-
-    return false; // Doesn't fall in any of the above cases
-}
-
 bool MyPolygon::contain(Point p, query_context *ctx){
 
 	if(ctx->is_within_query()){
@@ -144,18 +84,14 @@ bool MyPolygon::contain(Point p, query_context *ctx){
 		if(ctx->perform_refine||ctx->is_within_query()){
 			struct timeval border_start = get_cur_time();
 
+			// checking the intersection edges in the target pixel
 			for(int i = target->vstart; i < target->vend; i++) {
 				int j = i+1;
-//
-//				if(doIntersect(Point(boundary->x[i],boundary->y[i]),Point(boundary->x[j],boundary->y[j]),p,Point(target->high[0],p.y))){
-//					ret = !ret;
-//				}
-				if( ((boundary->y[i] >= p.y) != (boundary->y[j] >= p.y)) &&
-					(min(boundary->x[i],boundary->x[j])<=target->high[0]) &&
-					(max(boundary->x[i],boundary->x[j])>=p.x) &&
-					(p.x <= (boundary->x[j] - boundary->x[i]) * (p.y - boundary->y[i]) / (boundary->y[j] - boundary->y[i]) + boundary->x[i])){
-					ret = !ret;
-					//cout<<"intersected segment"<<endl;
+				if(((boundary->y[i] >= p.y) != (boundary->y[j] >= p.y))){
+					double int_x = (boundary->x[j] - boundary->x[i]) * (p.y - boundary->y[i]) / (boundary->y[j] - boundary->y[i]) + boundary->x[i];
+					if(p.x <= int_x && int_x <= target->high[0]){
+						ret = !ret;
+					}
 				}
 			}
 
@@ -168,19 +104,9 @@ bool MyPolygon::contain(Point p, query_context *ctx){
 			for(cross_info &info:partitions[target->id[0]][i].crosses[RIGHT]){
 				if(info.vertex<=p.y){
 					ret = !ret;
-					//cout<<"intersected node "<<i<<" "<<target->id[1]<<endl;
-
 				}
 			}
 		}
-
-//		if(ret!=contain(p)){
-//			lock();
-//			print();
-//			p.print();
-//			exit(0);
-//			unlock();
-//		}
 
 		return ret;
 	}else if(ctx->use_qtree){
