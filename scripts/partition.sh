e18d5a4094af9a06d24e27ff524687a494af3ec8
tengdj <tengdj@vip.qq.com>
2020-12-28 19:34:59 -0500

    minor update

---
+++ scripts/partition.sh
@@ -0,0 +1,11 @@
+
+../build/partition -s /data/gisdata/raster/dat/all_source_100.dat -v 10 -r  &&
+../build/partition -s /data/gisdata/raster/dat/all_source_100.dat -v 20 -r  &&
+../build/partition -s /data/gisdata/raster/dat/all_source_100.dat -v 30 -r  &&
+../build/partition -s /data/gisdata/raster/dat/all_source_100.dat -v 40 -r  &&
+../build/partition -s /data/gisdata/raster/dat/all_source_100.dat -v 50 -r  &&
+../build/partition -s /data/gisdata/raster/dat/all_source_100.dat -v 60 -r  &&
+../build/partition -s /data/gisdata/raster/dat/all_source_100.dat -v 70 -r  &&
+../build/partition -s /data/gisdata/raster/dat/all_source_100.dat -v 80 -r  &&
+../build/partition -s /data/gisdata/raster/dat/all_source_100.dat -v 90 -r  &&
+../build/partition -s /data/gisdata/raster/dat/all_source_100.dat -v 100 -r
