b51a2934f0e602305d1fb1a8bd97e98a73e52257
tengdj <tengdj@vip.qq.com>
2022-05-23 18:09:07 +0800

    minor update

--- sql/sqlserver.sql
+++ sql/sqlserver.sql
@@ -1,7 +1,7 @@
-create table polygon (id int not null, geog geography, constraint PK_T PRIMARY KEY (ID))
-create table polygon_target (id int not null, geog geography, constraint PK_T_target PRIMARY KEY (ID))
-create table polygon_parent (id int not null, geog geography, constraint PK_T_target PRIMARY KEY (ID))
-create table polygon_child (id int not null, geog geography, constraint PK_T_target PRIMARY KEY (ID))
+create table polygon (id int not null, geog geography, constraint PK_T PRIMARY KEY (ID));
+create table polygon_target (id int not null, geog geography, constraint PK_T_target PRIMARY KEY (ID));
+create table polygon_parent (id int not null, geog geography, constraint PK_T_parent PRIMARY KEY (ID));
+create table polygon_child (id int not null, geog geography, constraint PK_T_child PRIMARY KEY (ID));
 
 create table point (id int not null, geog geography, constraint PK_T PRIMARY KEY (ID))
 CREATE SPATIAL INDEX polygon_grid on polygon(geog) USING GEOGRAPHY_AUTO_GRID
@@ -13,14 +13,17 @@ CREATE SPATIAL INDEX grid_index on polygon(geog) USING GEOGRAPHY_AUTO_GRID
 
 CREATE SPATIAL INDEX grid_index ON polygon_small(geog) USING GEOGRAPHY_GRID WITH ( GRIDS = (HIGH, LOW, low, HIGH ), CELLS_PER_OBJECT = 640, PAD_INDEX  = ON ); 
 
-CREATE SPATIAL INDEX polygon_grid ON polygon(geog) USING GEOGRAPHY_GRID WITH ( BOUNDING_BOX = ( xmin=-180, ymin=-90, xmax=180, ymax=90), GRIDS = (MEDIUM, LOW, MEDIUM, HIGH ), CELLS_PER_OBJECT = 640, PAD_INDEX  = ON );  
+CREATE SPATIAL INDEX polygon_grid ON polygon(geog) USING GEOGRAPHY_GRID WITH ( BOUNDING_BOX = ( xmin=-180, ymin=-90, xmax=180, ymax=90), GRIDS = (MEDIUM, LOW, MEDIUM, HIGH ), CELLS_PER_OBJECT = 8192, PAD_INDEX  = ON );  
 
 
 time sqlcmd -S localhost -U SA -P '$Terry08161043' -d tengdb -I -Q "select count(*) from point t1, polygon_small t2 where t2.geog.STContains(t1.geog)=1;"
 sqlcmd -S localhost -U SA -P '$Terry08161043' -d tengdb -I -i polygon.small.sql
 
-BULK INSERT polygon FROM '/home/teng/git/IDEAL/src/points.csv' WITH (FIELDTERMINATOR = '|', ROWTERMINATOR = '\n')
-
+BULK INSERT polygon_parent FROM '/home/teng/git/IDEAL/src/mssql.parent.csv' WITH (FIELDTERMINATOR = '|', ROWTERMINATOR = '\n');
 
 select t1.id from point t1, polygon t2
+
+
+
+CREATE SPATIAL INDEX polygon_parent_grid ON polygon_parent(geog) USING GEOGRAPHY_GRID WITH ( BOUNDING_BOX = ( xmin=-180, ymin=-90, xmax=180, ymax=90), GRIDS = (MEDIUM, LOW, MEDIUM, HIGH ), CELLS_PER_OBJECT = 8192, PAD_INDEX  = ON );  
 
--- src/datagen/geninsert.cpp
+++ src/datagen/geninsert.cpp
@@ -58,13 +58,13 @@ int main(int argc, char** argv) {
 
 			if(vm.count("sql")){
 				printf("INSERT INTO %s(id, geog) ",table_name.c_str());
-				printf("VALUES (%d, geography::STGeomFromText('%s', 4326).MakeValid());\n",poly->getid(), poly->to_string(false, true).c_str());
+				printf("VALUES (%d, geography::STGeomFromText('%s', 4326).MakeValid());\n",poly->getid(), poly->to_string(true, true).c_str());
 				if(index%100==0){
 					printf("go\n");
 				}
 			}else{
 				printf("%d'%s",poly->getid(),del.c_str());
-				poly->print_without_return(false, true);
+				poly->print_without_return(true, true);
 				printf("'\n");
 			}
 			delete poly;
